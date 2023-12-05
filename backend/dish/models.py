aa82d4ec89e0053fa391db16ab5df438a188d23a
Olexander Bryl <edlrian814@gmail.com>
2023-12-05 12:28:31 +0200

    Add Category model

--- backend/dish/models.py
+++ backend/dish/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
