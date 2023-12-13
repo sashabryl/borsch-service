133d391becdabec06a8d5ea5d0cdcebe76e05462
Olexander Bryl <edlrian814@gmail.com>
2023-12-13 17:19:47 +0200

    Install debug toolbar

--- backend/borsch_api/settings.py
+++ backend/borsch_api/settings.py
@@ -42,12 +42,14 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
     "rest_framework",
+    "debug_toolbar",
     "dish",
     "user",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -57,6 +59,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "borsch_api.urls"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 TEMPLATES = [
     {
