5b56a81bc0a0e25675c38523bac8410bf08afaee
Olexander Bryl <edlrian814@gmail.com>
2023-12-13 17:51:40 +0200

    Update requirements.txt

--- backend/dish/views.py
+++ backend/dish/views.py
@@ -1,6 +1,7 @@
 from django.db.models import Q
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .models import Region, Category, Dish
