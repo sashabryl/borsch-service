5021071cadb354928222fd2ce522f5c90b3725f3
arturiermolenko <arturiermolenko@gmail.com>
2023-12-13 21:38:14 +0100

    Cleaned migrations, changed media root path

--- backend/borsch_api/settings.py
+++ backend/borsch_api/settings.py
@@ -130,9 +130,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-MEDIA_ROOT = BASE_DIR / "vol/web/media"
+# MEDIA_ROOT = BASE_DIR / "vol/web/media"
 
 MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 STATIC_URL = "static/"
 
